e446a2d03f62403973a04c4561b1e8921a5089ac
Danilo <danilodegortes@gmail.com>
2020-11-23 16:06:47 +0100

    bonus 2 ordinato al click

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        funMeta:'',
         genreList: [],
         playlist: [],
         lookForGenre : ''
     },
     methods: {
-        sortedByYear: function(disc) {
+        sortedByYear(disc) {
             return disc.sort(function(a, b) {
             return a.year - b.year;
             });
-        }
+        },
     },
     mounted() {
 
             axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((risposta) => {
                 this.playlist = risposta.data.response;
+                this.funMeta = risposta.data.response;
 
+                //genero in automatico la lista per i generi musicali
                 this.playlist.forEach((item, i) => {
                     if (!this.genreList.includes(item.genre.toLowerCase())) {
                         this.genreList.push(item.genre.toLowerCase());
